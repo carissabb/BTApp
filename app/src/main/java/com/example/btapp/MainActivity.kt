7f3740292ea42775f1dd96d5e4ec0b2bd46cd6a9
Ishi Bansal <ishibansal@vt.edu>
2024-10-28 22:55:31 -0400

    now connects to tomtom but needs to show the map

--- app/src/main/java/com/example/btapp/MainActivity.kt
+++ app/src/main/java/com/example/btapp/MainActivity.kt
@@ -3,58 +3,45 @@ package com.example.btapp
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.btapp.ui.map.MapFragment
-import com.example.btapp.ui.planTrip.PlanTripFragment
-import com.example.btapp.ui.routes.RoutesFragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.example.btapp.databinding.ActivityMainBinding
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.xml.XmlMapper
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.example.btapp.databinding.AppBarMainBinding
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btApiService: BTApiService
-    private lateinit var binding: AppBarMainBinding
-
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = AppBarMainBinding.inflate(layoutInflater)
-        //setContentView(binding.root) //R.layout.activity_main
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // for nav menu
         // Set up NavHostFragment and NavController
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
         // Set up bottom navigation with NavController
-        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        NavigationUI.setupWithNavController(bottomNav, navController) //binding.bottomNavigation
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
 
-
-        // convert xml to json for api processing
+        // Set up Retrofit for API calls
         val xmlMapper = XmlMapper()
         val retrofit = Retrofit.Builder()
             .baseUrl("http://www.bt4uclassic.org/webservices/bt4u_webservice.asmx/")
-            .addConverterFactory(JacksonConverterFactory.create(xmlMapper)) // Use Jackson for XML
+            .addConverterFactory(JacksonConverterFactory.create(xmlMapper))
             .build()
 
         btApiService = retrofit.create(BTApiService::class.java)
-
         fetchBusRoutes()
     }
 
-    // get current routes
     private fun fetchBusRoutes() {
         val call = btApiService.getCurrentRoutes()
         call.enqueue(object : Callback<CurrentRoutesResponse> {
@@ -62,13 +49,9 @@ class MainActivity : AppCompatActivity() {
                 call: Call<CurrentRoutesResponse>,
                 response: Response<CurrentRoutesResponse>
             ) {
-                Log.e("MainActivity", "Message:$response")
                 if (response.isSuccessful) {
                     val routeResponse = response.body()
                     val jsonMapper = ObjectMapper()
-                    Log.d("MainActivity", "Response: $routeResponse")
-
-                    // Convert the XML response to JSON
                     val jsonString = jsonMapper.writeValueAsString(routeResponse)
                     Log.d("MainActivity", "JSON Response: $jsonString")
                 } else {
@@ -81,10 +64,4 @@ class MainActivity : AppCompatActivity() {
             }
         })
     }
-
-    // get arrival/departure times for buses
-    private fun fetchArrivalAndDepartureTimesForRoutes(){
-
-    }
-
 }
--- app/src/main/java/com/example/btapp/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/btapp/ui/map/MapFragment.kt
@@ -5,55 +5,63 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.ViewModelProvider
 import com.example.btapp.databinding.FragmentMapBinding
 import com.tomtom.sdk.map.display.MapOptions
-import com.tomtom.sdk.map.display.ui.MapFragment
-import com.tomtom.sdk.map.display.ui.MapReadyCallback
+import com.tomtom.sdk.map.display.ui.MapFragment as TomTomMapFragment
 import com.tomtom.sdk.map.display.TomTomMap
 import com.example.btapp.R
 
-
-class MapFragment : Fragment() {
+class CustomMapFragment : Fragment() { // renamed to avoid name conflict
 
     private var _binding: FragmentMapBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
-     override fun onCreateView(
+
+    override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        Log.d("MapFragment", "MapFragment is loaded")
-
-        val mapOptions = MapOptions(mapKey = "Z8uScvfGWAph6kefoGIYKRNeUYJGhi8G")
-        val mapFragment = MapFragment.newInstance(mapOptions)
-
-        childFragmentManager.beginTransaction()
-            .replace(R.id.map_fragment, mapFragment)
-            .commit()
-
-         mapFragment.getMapAsync { tomtomMap: TomTomMap ->
-             val tomtomMap = tomtomMap
-         }
-         _binding = FragmentMapBinding.inflate(inflater, container, false)
-         return binding.root
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Your map initialization logic here
+
+        Log.d("CustomMapFragment", "MapFragment is loaded")
+
+        val mapOptions = MapOptions(mapKey = "Z8uScvfGWAph6kefoGIYKRNeUYJGhi8G")
+        val tomTomMapFragment = TomTomMapFragment.newInstance(mapOptions)
+
+        // Add the TomTomMapFragment to the container in FragmentMap layout
+        childFragmentManager.beginTransaction()
+            .replace(R.id.map_fragment_container, tomTomMapFragment) // use correct ID from FragmentMap
+            .commit()
+
+        // Initialize the map once the TomTomMapFragment is ready
+        tomTomMapFragment.getMapAsync { tomtomMap: TomTomMap ->
+            Log.d("CustomMapFragment", "TomTom Map is initialized")
+            initializeMap(tomtomMap)
+        }
     }
+
+    private fun initializeMap(tomTomMap: TomTomMap) {
+        // Example coordinates and zoom level
+        val latitude = 37.7749
+        val longitude = -122.4194
+        val zoomLevel = 12.0
+
+        // Center the map
+        tomTomMap.centerOn(latitude, longitude)
+
+        // Set the zoom level
+        tomTomMap.zoomTo(zoomLevel)
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
